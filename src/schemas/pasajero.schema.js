e17a37b0150f9bafb6758dc9f5e86d549a861cb2
andres10281 <162849367+andres10281@users.noreply.github.com>
2025-01-15 19:09:55 -0500

    Update pasajero.schema.js

--- src/schemas/pasajero.schema.js
+++ src/schemas/pasajero.schema.js
@@ -3,25 +3,25 @@ import { z } from "zod";
 export const createPasajeroSchema = z.object({
   cedula: z
     .number({
-      required_error: "El documento es requerido", // Corrección aquí
+      required_error: "El documento es requerido", 
     }),
   nombre: z
     .string({
-      required_error: "El nombre es requerido", // Corrección aquí
+      required_error: "El nombre es requerido", 
     }),
   apellido: z
     .string({
-      required_error: "El apellido es requerido", // Corrección aquí
+      required_error: "El apellido es requerido", 
     }),
   telefono: z
     .string({
-      required_error: "El teléfono es requerido", // Corrección aquí
+      required_error: "El teléfono es requerido", 
     }),
   email: z
     .string({
-      required_error: "El email es requerido", // Corrección aquí
+      required_error: "El email es requerido",
     })
     .email({
-      message: "El email no es válido", // Validación de formato
+      message: "El email no es válido", 
     }),
 });
